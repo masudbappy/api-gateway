33b7b3b284369d4f18c9cad56c1be9ec740f63b4
masudbappy <masud.bappy90@gmail.com>
2023-01-15 21:56:16 +0600

    Added custom postFilter.

--- src/main/java/com/masudbappy/apigateway/ApiGatewayApplication.java
+++ src/main/java/com/masudbappy/apigateway/ApiGatewayApplication.java
@@ -3,9 +3,11 @@ package com.masudbappy.apigateway;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
 @EnableEurekaClient
+@ComponentScan("com.masudbappy.filter")
 public class ApiGatewayApplication {
 
 	public static void main(String[] args) {
--- src/main/java/com/masudbappy/filter/CustomFilter.java
+++ src/main/java/com/masudbappy/filter/CustomFilter.java
@@ -6,6 +6,7 @@ import org.springframework.cloud.gateway.filter.GatewayFilterChain;
 import org.springframework.cloud.gateway.filter.GlobalFilter;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.server.reactive.ServerHttpRequest;
+import org.springframework.http.server.reactive.ServerHttpResponse;
 import org.springframework.web.server.ServerWebExchange;
 import reactor.core.publisher.Mono;
 
@@ -15,7 +16,18 @@ public class CustomFilter implements GlobalFilter {
     @Override
     public Mono<Void> filter(ServerWebExchange exchange, GatewayFilterChain chain) {
         ServerHttpRequest request = exchange.getRequest();
-        logger.info("Authorization is + " + request.getHeaders().getFirst("Authorization"));
-        return chain.filter(exchange);
+
+        if (request.getURI().toString().contains("/api/student/")) {
+
+        }
+
+        logger.info("Authorization = " + request.getHeaders().getFirst("Authorization"));
+
+        return chain.filter(exchange).then(Mono.fromRunnable(() -> {
+            ServerHttpResponse response = exchange.getResponse();
+
+            logger.info("Post Filter = " + response.getStatusCode());
+        }));
     }
+
 }
